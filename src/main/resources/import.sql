40eb4afbca74b90f624571d01a6f14648e28f018
Thiago Barbosa <thiago.fbarbosa@grupomateus.com>
2024-01-21 20:57:08 -0300

    Removemdo inserts desnecessários

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -6,6 +6,3 @@ INSERT INTO customer (name, email, gender) VALUES ('Gamora', 'gamora@vingadores.
 
 INSERT INTO address (cep, logradouro, complemento, bairro, localidade, uf, customer_id) VALUES ('22785600', 'Rua Manhuacu, 53', 'Casa 12', 'Vargem Grande', 'Rio de Janeiro', 'RJ', 1);
 INSERT INTO address (cep, logradouro, complemento, bairro, localidade, uf, customer_id) VALUES ('54336433', 'Rua 3, 122', NULL, 'Bairro 1', 'Belford Roxo', 'RJ', 1);
-
-INSERT INTO user(USERNAME, username) VALUES ('admin', 'passwd');
-INSERT INTO user(username, username) VALUES ('user', 'userpwd');
